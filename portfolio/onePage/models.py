d678fbfbbe6c8e556df43e1f67637d57f80b3d37
wookim789 <wookim789@gmail.com>
2018-11-05 12:06:37 +0900

    맵 추가하기 전

--- portfolio/onePage/models.py
+++ portfolio/onePage/models.py
@@ -3,8 +3,40 @@ from django.db import models
 # Create your models here.
 
 
+class UserCount(models.Model):
+    userId = models.CharField(max_length=10)
+    userPw = models.CharField(max_length=20)
+    email = models.CharField(max_length=25)
+    teamCount = models.IntegerField()
+
+
+class TeamInfo(models.Model):
+    teamName = models.CharField(max_length=10)
+    userId = models.CharField(max_length=10)
+    leader = models.IntegerField()
+
+
+class TemaPlanList(models.Model):
+    teamName = models.CharField(max_length=10)
+    teamPlanNo = models.IntegerField()
+    teamPlanName = models.CharField(max_length=10)
+
+
 class SelectDate(models.Model):
+    teamPlanNo = models.IntegerField()
     userId = models.CharField(max_length=10)
     selectDate = models.CharField(max_length=10)
     dateCount = models.IntegerField(default=0)
     confirmIndicator = models.IntegerField(default=0)
+
+
+class PlanMap(models.Model):
+    teamPlanNo = models.IntegerField()
+    selectDate = models.CharField(max_length=10)
+    userId = models.CharField(max_length=10)
+    pickCount = models.IntegerField()
+    userColor = models.CharField(max_length=10)
+    lat = models.IntegerField()
+    lng = models.IntegerField()
+    address = models.CharField(max_length=10)
+    confirm = models.IntegerField()
